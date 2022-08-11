c96b954de3444e27aa96f0e7fcd421a9b547e71b
Calixto <calixto.lopez.santos@gmail.com>
2022-08-12 00:54:16 +0200

    Add correct rubocop rules
     - Aplication rules in game_of_life.rb
    
    Co-authored-by: BriannMorales <moralesbrayitan@gmail.com>
    Co-authored-by: GustavoHiguera <ghiguera@ucol.mx>

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,17 +1,19 @@
-class Board
-    def initialize(row, column)
-       @board =[]
-       row.times do 
-        @board.push(Array.new(column,"."))
-       end
-    end   
-    
-    def show_board
-        @board.each do |row| 
-            puts row.join(" ")
-        end
-    end
-end
+# frozen_string_literal: true
 
-board_instance = Board.new(5,5)
+# class create Board with n rows and n columns
+class Board
+  def initialize(row, column)
+    @board = []
+    row.times do
+      @board.push(Array.new(column, '.'))
+    end
+  end
+
+  def show_board
+    @board.each do |row|
+      puts row.join(' ')
+    end
+  end
+end
+board_instance = Board.new(5, 5)
 board_instance.show_board
