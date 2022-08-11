b443ab5d0dca9bf2f3c7d98817f1942ab52730b5
Calixto <calixto.lopez.santos@gmail.com>
2022-08-12 01:32:33 +0200

    Add methods AlliveCell  - Get number alive cells
    Co-authored-by: BriannMorales <moralesbrayitan@gmail.com>
    Co-authored-by: GustavoHiguera <ghiguera@ucol.mx>

--- game_of_life.rb
+++ game_of_life.rb
@@ -17,3 +17,29 @@ class Board
 end
 board_instance = Board.new(5, 5)
 board_instance.show_board
+
+# Class for play
+
+class AliveCell
+  def initialize(number_alive_cells)
+    @number_alive_cells = number_alive_cells
+  end
+  def alive_cells
+    puts " #{@number_alive_cells}  celulas vivas"
+  end  
+end
+print 'Ingresa el número de celulas vivas: '
+number_alive_cells_input = gets.chomp
+total_cells_alive = AliveCell.new(number_alive_cells_input)
+total_cells_alive.alive_cells
+
+class RandomAliveCell
+  def initialize(number_rand)
+    @number_rand = number_rand
+  end
+#   def random
+#     row = rand(0..)
+#     column =rand(0..)
+#     array = push[[1 ,2] ,  [2 ,5], [4 ,y], [x ,y], [x ,y] ]
+#   end
+end
