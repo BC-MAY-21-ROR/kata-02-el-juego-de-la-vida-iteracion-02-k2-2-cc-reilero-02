734610b25209bd49eb3c916523eec9489f3d915c
GustavoHiguera <ghiguera@ucol.mx>
2022-08-11 19:02:56 -0500

    Add rows and columns input
    Co-authored-by: CalixtoLopez <calixto.lopez.santos@gmail.com>
    Co-authored-by: BriannMorales <moralesbrayitan@gmail.com>

--- game_of_life.rb
+++ game_of_life.rb
@@ -5,7 +5,7 @@ class Board
   def initialize(row, column)
     @board = []
     row.times do
-      @board.push(Array.new(column, '.'))
+      @board.push(Array.new(column, RandomAliveCell.new()))
     end
   end
 
@@ -15,31 +15,43 @@ class Board
     end
   end
 end
-board_instance = Board.new(5, 5)
-board_instance.show_board
 
-# Class for play
-
+# class AliveCell puts the cells into the board
 class AliveCell
   def initialize(number_alive_cells)
     @number_alive_cells = number_alive_cells
   end
+
   def alive_cells
     puts " #{@number_alive_cells}  celulas vivas"
-  end  
+  end
 end
+
+# class RandomAliveCell defines which index in the array will have an alive cell.
+# class RandomAliveCell
+#   def initialize(number_rand)
+#     @number_rand = number_rand
+#   end
+  
+#   # def random 
+#   #   number_alive_cells_input.times do
+#   #   random_row=rand(number_of_rows)
+#   #   random_column=rand(number_of_columns)
+#   #   put_alive_cell(random_row, random_column)
+#   #   end
+#   # end
+# end
+
+print 'Ingresa el número de filas del tablero: '
+number_of_rows = gets.chomp.to_i
+print 'Ingresa el número de columnas del tablero: '
+number_of_columns = gets.chomp.to_i
+
 print 'Ingresa el número de celulas vivas: '
-number_alive_cells_input = gets.chomp
+number_alive_cells_input = gets.chomp.to_i
 total_cells_alive = AliveCell.new(number_alive_cells_input)
+
+board_instance = Board.new(number_of_rows, number_of_columns)
+board_instance.show_board
 total_cells_alive.alive_cells
 
-class RandomAliveCell
-  def initialize(number_rand)
-    @number_rand = number_rand
-  end
-#   def random
-#     row = rand(0..)
-#     column =rand(0..)
-#     array = push[[1 ,2] ,  [2 ,5], [4 ,y], [x ,y], [x ,y] ]
-#   end
-end
